de3a5fa00706e2d02b6337f0191fd3348fe9f91d
Eder <ezapataa1@eafit.edu.co>
2022-05-28 19:27:42 -0500

    Biseccion Incompleto

--- AnalisisNumerico/views.py
+++ AnalisisNumerico/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from .models import Metodo
 
+from sympy import *
+
 # Create your views here.
 
 def home(request):
@@ -16,6 +18,28 @@ def methods(request):
 	else:
 		metodos = Metodo.objects.all()
 	return render(request, 'methods.html', {'searchTerm':searchTerm, 'metodos':metodos})
+
+def biseccion(request):
+	x = symbols('x') 
+	exp = sin(x)
+	xi = 0
+	xs = 1
+	_type = 0
+	tol = 0.5*10**-3
+	_iter = 10
+	
+	if request.method == 'POST':
+		exp = request.POST.get('txtFUN')
+		xi = request.POST.get('txtXI')
+		xs = request.POST.get('txtXS')
+		_type = request.POST.get('txtTYPE')
+		tol = request.POST.get('txtTOL')
+		_iter = request.POST.get('txtITER')
+
+	f = lambdify(x, exp, "math")
+
+	return render(request, 'biseccion.html')
+		
 
 def about(request):
 	return HttpResponse('<h1>Holaaaa</h1>')
--- django_project/urls.py
+++ django_project/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', ANviews.home),
     path('about/', ANviews.about),
     path('methods/', ANviews.methods),
+    path('Biseccion/', ANviews.biseccion),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
